2c449271ac5a8ed52d8e09870fd3701c5e5e5907
EdwinDiaz93 <josephph1993@gmail.com>
2023-07-29 18:41:08 -0600

    add types to headers

--- src/api/factory.ts
+++ src/api/factory.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosRequestHeaders } from 'axios';
 import { Http } from '../models/http.model';
 
 class BaseRepository {
@@ -8,8 +8,8 @@ class BaseRepository {
         this.apiUrl = process.env.REACT_APP_APIBASE_URL || ''; // Obtenemos la URL de la API desde las variables de entorno
     }
 
-    protected async request<T, U>(method: Http, path: string, data: T, headers?: any): Promise<U> {
-        try {
+    protected async request<T, U>(method: Http, path: string, data: T, headers?: AxiosRequestHeaders): Promise<U> {
+        try {            
             const response: AxiosResponse<U> = await axios.request<U>({
                 method,
                 url: `${this.apiUrl}/${path}`,
